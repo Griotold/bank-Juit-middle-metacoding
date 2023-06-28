f831bd54572de1e5516d5a9fec491e63427f49ad
Griotold <gotjd9773@naver.com>
2023-06-28 12:56:11 +0900

    섹션 2. 스프링부트 회원가입 - 회원가입 컨트롤러 테스트

---
+++ src/test/java/shop/mtcoding/bank/web/UserControllerTest.java
@@ -0,0 +1,93 @@
+package shop.mtcoding.bank.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.user.UserReqDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static shop.mtcoding.bank.dto.user.UserReqDto.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+class UserControllerTest extends DummyObject{
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        dataSetting();
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void join_success_test() throws Exception {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("love");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("love@nate.com");
+        joinReqDto.setFullname("러브");
+
+        String requestBody = om.writeValueAsString(joinReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        // when
+        ResultActions resultActions
+                = mvc.perform(post("/api/join").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody = " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 테스트")
+    void join_fail_test() throws Exception {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("ssar");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("ssar@nate.com");
+        joinReqDto.setFullname("쌀");
+
+        String requestBody = om.writeValueAsString(joinReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        // when
+        ResultActions resultActions
+                = mvc.perform(post("/api/join").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody = " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+
+    private void dataSetting(){
+        userRepository.save(newUser("ssar", "쌀"));
+    }
+}
