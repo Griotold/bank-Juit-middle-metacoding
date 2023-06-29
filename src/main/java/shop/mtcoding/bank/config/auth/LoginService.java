7d2144c1991130d17f33be50d9b951c71fae0261
Griotold <gotjd9773@naver.com>
2023-06-29 16:57:00 +0900

    3. 스프링부트 JWT 인증과 인가 - JWT 토큰 생성을 위한 세팅

---
+++ src/main/java/shop/mtcoding/bank/config/auth/LoginService.java
@@ -0,0 +1,30 @@
+package shop.mtcoding.bank.config.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+
+@Service
+public class LoginService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 시큐리티로 로그인이 될때, 시큐리티가 아래 메소드를 실행
+    // DB에서 username을 체크
+    // 없으면 오류 -> 무조건 InternalAuthenticationServiceException 예외를 터트려야함
+    // 있으면 정상적으로 시큐리티 컨텍스트 내부 세션에 로그인된 세션이 만들어진다.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userPS = userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new InternalAuthenticationServiceException("인증 실패")
+                );
+        return new LoginUser(userPS);
+    }
+}
