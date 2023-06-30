7f422f99828ccf78d7f5814f694245850ee9fce0
Griotold <gotjd9773@naver.com>
2023-06-30 19:05:50 +0900

    3. 스프링부트 JWT 인증과 인가 - unsuccessfulAuthentication 테스트

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static shop.mtcoding.bank.dto.user.UserReqDto.*;
+@Transactional
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -64,18 +66,31 @@ class JwtAuthenticationFilterTest extends DummyObject {
         assertNotNull(jwtToken);
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
         resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
-
-
     }
 
     @Test
-    @DisplayName("")
-    void unsuccessfulAuthentication_test() {
+    @DisplayName("인증 실패 상황")
+    void unsuccessfulAuthentication_test() throws Exception {
         // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar1");
+        loginReqDto.setPassword("1234");
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
 
         // when
+        ResultActions resultActions = mvc.perform(
+                post("/api/login").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody = " + responseBody);
+
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("jwtToken = " + jwtToken);
 
         // then
+        resultActions.andExpect(status().isUnauthorized());
+        assertNull(jwtToken);
     }
 
 }
