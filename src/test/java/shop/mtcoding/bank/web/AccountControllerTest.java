a1af5601303eaf91b025c1fdd0de778779380d50
Griotold <gotjd9773@naver.com>
2023-07-03 13:29:27 +0900

    4. Bank 애플리케이션 - 계좌 컨트롤러 테스트 AccountControllerTest

---
+++ src/test/java/shop/mtcoding/bank/web/AccountControllerTest.java
@@ -0,0 +1,78 @@
+package shop.mtcoding.bank.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.account.AccountReqDto;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static shop.mtcoding.bank.dto.account.AccountReqDto.*;
+
+@ActiveProfiles("test")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+class AccountControllerTest extends DummyObject {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User ssar = userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    // setBefore = TEST_METHOD : setUp()메소드 수행전에
+    // setBefore = TEST_EXECUTION : saveAccount_test() 수행전에
+    @WithUserDetails(value = "ssar", setupBefore = TestExecutionEvent.TEST_EXECUTION) // 디비에서 ssar을 조회해서 세션에 담아주는 어노테이션
+    @Test
+    @DisplayName("컨트롤러의 saveAccount() 테스트")
+    void saveAccount_test() throws Exception {
+        // given
+        AccountSaveReqDto accountSaveReqDto = new AccountSaveReqDto();
+        accountSaveReqDto.setNumber(9999L);
+        accountSaveReqDto.setPassword(1234L);
+        String requestBody = om.writeValueAsString(accountSaveReqDto);
+        System.out.println("테스트 = " + requestBody);
+
+        // when
+        ResultActions resultActions
+                = mvc.perform(post("/api/s/account")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        System.out.println("테스트 = " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isCreated());
+    }
+}
