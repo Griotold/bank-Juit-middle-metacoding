7f9aeb49e4c53fdd0214903a4bc94bf74472d57c
Griotold <gotjd9773@naver.com>
2023-07-03 11:44:13 +0900

    4. Bank 애플리케이션 - AccountService

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -2,5 +2,9 @@ package shop.mtcoding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    // todo : 리팩토링해야함
+    Optional<Account> findByName(Long number);
 }
