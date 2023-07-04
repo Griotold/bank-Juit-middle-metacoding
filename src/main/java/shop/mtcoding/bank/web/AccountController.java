a4f855c5dca106b91528bb0b2f3981c59b5aed97
Griotold <gotjd9773@naver.com>
2023-07-04 11:03:54 +0900

    4. Bank 애플리케이션 - 본인계좌목록 컨트롤러

--- src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -7,14 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.account.AccountReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto;
+import shop.mtcoding.bank.handler.ex.CustomForbiddenException;
 import shop.mtcoding.bank.service.AccountService;
 
 import javax.validation.Valid;
@@ -37,5 +35,36 @@ public class AccountController {
                                          @AuthenticationPrincipal LoginUser loginUser) {
         AccountSaveRespDto accountSaveRespDto = accountService.register(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌 등록 성공", accountSaveRespDto), HttpStatus.CREATED);
+    }
+    /**
+     * V1 : userId를 쿼리파라미터로 받는 경우
+     * userId와 로그인된 유저의 id가 일치하는지 검증이 필요하다.
+     * */
+    @GetMapping("/s/account/{userId}")
+    public ResponseEntity<?> findUserAccountListV1(@PathVariable Long userId,
+                                                   @AuthenticationPrincipal LoginUser loginUser) {
+        if (userId != loginUser.getUser().getId()) {
+            throw new CustomForbiddenException("권한이 없습니다");
+        }
+        AccountListRespDto accountListRespDto = accountService.accountList(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
+    }
+
+    /**
+     * V2 : 인증이 필요하고 account 다 주세요!
+     * URL 경로가 애매하다. URL만 보면 모든 회원의 계좌를 줘야할 것만 같다.
+     */
+    @GetMapping("/s/account")
+    public ResponseEntity<?> findUserAccountListV2(@AuthenticationPrincipal LoginUser loginUser) {
+        AccountListRespDto accountListRespDto = accountService.accountList(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
+    }
+    /**
+     * V3 : 그냥 로그인한 유저의 계좌를 주세요
+     * */
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccountListV3(@AuthenticationPrincipal LoginUser loginUser) {
+        AccountListRespDto accountListRespDto = accountService.accountList(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
     }
 }
