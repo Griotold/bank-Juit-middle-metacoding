fa0c3a21a7e460a2407fca05885f18b413788911
Griotold <gotjd9773@naver.com>
2023-07-06 16:48:55 +0900

    4. Bank 애플리케이션 - Long Type 테스트

--- src/test/java/shop/mtcoding/bank/temp/LongTest.java
+++ src/test/java/shop/mtcoding/bank/temp/LongTest.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.bank.temp;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +33,29 @@ public class LongTest {
         } else {
             System.out.println("테스트 = 동일하지 않습니다.");
         }
+    }
+
+    @Test
+    @DisplayName("2의 8제곱까지는 단순 비교 가능")
+    void smallValue_test() {
+        // given
+        Long num1 = 127L;
+        Long num2 = 127L;
+
+        if (num1 == num2) {
+            System.out.println("같습니다");
+        } else {
+            System.out.println("다릅니다.");
+        }
+    }
+    @Test
+    @DisplayName("assertThat().isEqualTo()는 큰값도 비교 가능")
+    void assertThat_isEqualTo_test() {
+        // given
+        Long num1 = 1270L;
+        Long num2 = 1270L;
+
+        // then
+        Assertions.assertThat(num1).isEqualTo(num2);
     }
 }
