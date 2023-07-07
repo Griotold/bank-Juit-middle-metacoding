d49a73fcce7b023b7d678b69824b69434f46f124
Griotold <gotjd9773@naver.com>
2023-07-07 09:56:18 +0900

    4. Bank 애플리케이션 - @DataJpaTest autoincrement 초기화

--- src/test/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImplTest.java
+++ src/test/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImplTest.java
@@ -1,15 +1,22 @@
 package shop.mtcoding.bank.domain.transaction;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("test")
 @DataJpaTest // DB 관련된 Bean이 다 올라온다.
 class TransactionRepositoryImplTest extends DummyObject {
 
@@ -22,9 +29,41 @@ class TransactionRepositoryImplTest extends DummyObject {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private EntityManager em;
+
     @BeforeEach
     public void setUp() {
+        autoincrementReset();
         dataSetting();
+    }
+
+    @Test
+    @DisplayName("거래 내역 리스트 조회1")
+    void transaction_list_test() {
+        // given
+        List<Transaction> all = transactionRepository.findAll();
+        all.forEach((t) -> {
+            System.out.println("테스트 t.getId() = " + t.getId());
+            System.out.println("테스트 t.getSender() = " + t.getSender());
+            System.out.println("테스트 t.getReceiver() = " + t.getReceiver());
+            System.out.println("테스트 t.getGubun() = " + t.getGubun());
+            System.out.println("=====================");
+        });
+    }
+
+    @Test
+    @DisplayName("거래 내역 리스트 조회2")
+    void transaction_list_test2() {
+        // given
+        List<Transaction> all = transactionRepository.findAll();
+        all.forEach((t) -> {
+            System.out.println("테스트 t.getId() = " + t.getId());
+            System.out.println("테스트 t.getSender() = " + t.getSender());
+            System.out.println("테스트 t.getReceiver() = " + t.getReceiver());
+            System.out.println("테스트 t.getGubun() = " + t.getGubun());
+            System.out.println("=====================");
+        });
     }
     private void dataSetting() {
         User ssar = userRepository.save(newUser("ssar", "쌀"));
@@ -47,6 +86,13 @@ class TransactionRepositoryImplTest extends DummyObject {
                 .save(newTransferTransaction(ssarAccount1, loveAccount, accountRepository));
         Transaction transferTransaction3 = transactionRepository
                 .save(newTransferTransaction(cosAccount, ssarAccount1, accountRepository));
+    }
+
+    private void autoincrementReset() {
+
+        em.createNativeQuery("ALTER TABLE user_tb ALTER COLUMN id RESTART WITH 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE account_tb ALTER COLUMN id RESTART WITH 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE transaction_tb ALTER COLUMN id RESTART WITH 1").executeUpdate();
     }
 
 }
